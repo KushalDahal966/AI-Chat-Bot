46046cbc0dbc612e07df92fa690897af7e4dff13
Kushal Dahal <kushaldahal761@gmail.com>
2026-01-25 21:55:59 +0545

    fixed mobile keyboard overlap on input box focus

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
+
+const chatSection = document.querySelector(".chat-section");
+const promptContainer = document.querySelector(".prompt-section-container");
+const imageInputBtn = document.querySelector(".user-input-image");
+const imageInput = document.querySelector("#input-image");
+const inputArea = document.querySelector(".user-input-text");
+const userInput = document.querySelector("#input-text");
+const sendBtn = document.querySelector(".send-button");
+
+userInput.addEventListener("focus", () => {
+    setTimeout(() => {
+        promptContainer.scrollIntoView({ behavior: "smooth", block: "nearest" });
+    }, 300);
+});
