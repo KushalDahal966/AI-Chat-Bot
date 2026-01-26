3b958472bfcd6693d9c7bec4a3d4969cd0bf1a4e
Kushal Dahal <kushaldahal761@gmail.com>
2026-01-26 18:06:10 +0545

    created user chat bubble and add loading state to ai chat bubble until response is fetched

--- src/main.js
+++ src/main.js
@@ -8,6 +8,36 @@ const inputArea = document.querySelector(".user-input-text");
 const userInput = document.querySelector("#input-text");
 const sendBtn = document.querySelector(".send-button");
 
+const showLoading = () => {
+    const aiChatBubble = document.createElement("div");
+    aiChatBubble.classList.add("ai-chat-section");
+    aiChatBubble.innerHTML = `<div class="ai-chat-section">
+    <img src="src/images/chat-bot.png" alt="ai image"
+    class="ai-image"/> <p class="ai-chat-text"><i class="fa-solid fa-spinner fa-spin"></i>
+    </p> </div>`;
+    chatSection.append(aiChatBubble);
+};
+
+const showChatBubble = () => {
+    const userChatBubble = document.createElement("div");
+    userChatBubble.classList.add("user-chat-section");
+    userChatBubble.innerHTML = `<img src="src/images/user.png" alt="user image" class="user-image" />
+    <p class="user-chat-text">${userInput.value}</p>`
+    chatSection.append(userChatBubble);
+    userInput.value = "";
+};
+
+const getResponse = async () => {
+    showChatBubble();
+    showLoading();
+};
+hi
+sendBtn.addEventListener("click", getResponse);
+userInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") getResponse();
+    return;
+});
+
 userInput.addEventListener("focus", () => {
     setTimeout(() => {
         promptContainer.scrollIntoView({ behavior: "smooth", block: "nearest" });
