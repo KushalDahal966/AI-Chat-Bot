fc49a5da2f08fa27e2d689e093d52b763e6b8428
Kushal Dahal <kushaldahal761@gmail.com>
2026-01-26 22:26:35 +0545

    added functionality to show uploaded image preview

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ const API_KEY = import.meta.env.VITE_API_KEY;
 
 const chatSection = document.querySelector(".chat-section");
 const promptContainer = document.querySelector(".prompt-section-container");
+const imagePreview = document.querySelector(".image-preview");
+const imageCloseBtn = document.querySelector(".image-close-button");
 const imageInputBtn = document.querySelector(".user-input-image");
 const imageInput = document.querySelector("#input-image");
-const inputArea = document.querySelector(".user-input-text");
+const inputArea = document.querySelector(".user-input-field");
 const userInput = document.querySelector("#input-text");
 const sendBtn = document.querySelector(".send-button");
 
@@ -68,8 +70,38 @@ userInput.addEventListener("keydown", (e) => {
     return;
 });
 
+imageCloseBtn.addEventListener("click", () => {
+    if (imageUrl) {
+        URL.revokeObjectURL(imageUrl);
+        imageUrl = null;
+    }
+
+    imagePreview.src = "";
+    imagePreview.style.display = "none";
+    imageCloseBtn.style.display = "none";
+    imageInputBtn.disabled = false;
+    imageInputBtn.style.opacity = 1;
+    imageInputBtn.style.cursor = "pointer";
+
+})
+
+let imageUrl = null;
+imageInput.addEventListener("change", () => {
+    const file = imageInput.files[0];
+
+    if (!file) return;
+
+    imageUrl = URL.createObjectURL(file)
+    imagePreview.src = imageUrl;
+    imagePreview.style.display = "block";
+    imageCloseBtn.style.display = "block";
+    imageInputBtn.disabled = true;
+    imageInputBtn.style.opacity = 0.5;
+    imageInputBtn.style.cursor = "not-allowed";
+})
+
 imageInputBtn.addEventListener("click", () => {
-    alert("Wrok In Progress!");
+    imageInput.click();
 })
 
 userInput.addEventListener("focus", () => {
